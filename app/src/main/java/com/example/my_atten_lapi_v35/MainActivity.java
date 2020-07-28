114b983bd30e7733299233ed5d7c7d5a26c95411
Error57nk <nkkumarnk7@gmail.com>
2020-07-28 14:32:27 +0530

    register-l

---
+++ app/src/main/java/com/example/my_atten_lapi_v35/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.my_atten_lapi_v35;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    public void appLinks(View view){
+
+        //Get url from tag
+        String url = view.getTag().toString();
+        Log.i("tagInfo", view.getTag().toString());
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+        //pass the url to intent data
+        intent.setData(Uri.parse(url));
+
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
